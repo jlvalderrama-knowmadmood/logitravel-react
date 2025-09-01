9091177019a35907e1dd5441ee1f158a0b2e7ea4
Pepe <pepe@Pepes-MacBook-Pro.local>
2025-09-01 11:12:03 +0200

    feat: create markup

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,72 @@
+import { useState } from "react";
+import { RiResetRightFill } from "react-icons/ri";
+
 function App() {
+  const [items, setItems] = useState<string[]>([]);
+  const [newItemValue, setNewItemValue] = useState<string>("");
+  const [isAddNewItemModalVisible, setAddNewItemModalVisible] =
+    useState<boolean>(false);
+
+  function handleNewItemValueChange(
+    event: React.ChangeEvent<HTMLInputElement>
+  ) {}
+
+  function handleAddItemSubmit(event: React.FormEvent) {}
+
+  function handleOpenAddNewItemModal() {}
+
+  function handleCancelProcess(event: React.MouseEvent<HTMLButtonElement>) {}
+
   return (
-    <div>
-      <h1>Prueba técnica Logitravel</h1>
-    </div>
+    <main>
+      <div className="surface">
+        <div className="surface__header">
+          <h1 className="title-1">This is a technical proof</h1>
+          <p className="text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos in
+            recusandae, corporis ullam debitis cumque cum tempora ipsa. Quasi,
+            accusamus?
+          </p>
+        </div>
+        {items.length === 0 ? (
+          <p>No items yet</p>
+        ) : (
+          <ul className="list">
+            {items.map((item) => (
+              <li className="list__item" key={item}>
+                {item}
+              </li>
+            ))}
+          </ul>
+        )}
+        <div className="actions">
+          <button className="button">
+            <RiResetRightFill />
+          </button>
+          <button className="button">Delete</button>
+          <button className="button" onClick={handleOpenAddNewItemModal}>
+            Add
+          </button>
+        </div>
+        {isAddNewItemModalVisible ? (
+          <div className="add-item">
+            <h2 className="title-2">Add item to list</h2>
+            <form onSubmit={handleAddItemSubmit} className="add-new-item-form">
+              <input
+                type="text"
+                placeholder="Type the text here..."
+                value={newItemValue}
+                onChange={handleNewItemValueChange}
+              />
+              <div className="add-new-item-form__actions">
+                <button onClick={handleCancelProcess}>Cancel</button>
+                <button onClick={handleAddItemSubmit}>Add</button>
+              </div>
+            </form>
+          </div>
+        ) : null}
+      </div>
+    </main>
   );
 }
 
